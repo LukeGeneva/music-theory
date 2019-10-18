c828f3b2f14b77e0e922ee558751554d85d7700f
Luke Geneva <lukegeneva@gmail.com>
2019-10-18 15:25:28 -0500

    Include all major keys

--- src/keys.js
+++ src/keys.js
@@ -1,1 +1,17 @@
-export const MAJOR_KEYS = ['C', 'G', 'D', 'A', 'E', 'B', 'F#', 'C#'];
+export const MAJOR_KEYS = [
+  'C',
+  'G',
+  'D',
+  'A',
+  'E',
+  'B',
+  'F#',
+  'C#',
+  'F',
+  'Bb',
+  'Eb',
+  'Ab',
+  'Db',
+  'Gb',
+  'Cb'
+];
